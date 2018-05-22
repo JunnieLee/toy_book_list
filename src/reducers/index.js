73a532a1f7167107b669fdcbfdbb6544d8dcc6ee
JunnieLee <heunghari@naver.com>
2018-05-22 16:13:05 +0900

    generated books_reducer and wired it up with the app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
+					// 이 combineReducers가 state를 mapping함!
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
